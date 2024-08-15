9dbc58f9707dd224b6ee614abf460e936f12814e
Mohammad Zishan Ansari <azishanansari4587@gmail.com>
2024-08-16 03:30:00 +0530

    page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Home } from './pages/Home';
 
 function App() {
 
@@ -36,6 +37,7 @@ function App() {
   return (
     <>
     <h1>Zishan Ansari</h1>
+    <Home/>
       <RouterProvider router={route}/>   
     </>
   )
